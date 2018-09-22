cf3a40cefb507b962f6d987a1b5a2e4748ceaf7e
Pete Houston <contact@petehouston.com>
2018-09-22 20:37:17 +0700

    Rename cli config key to headers.

--- bin/crawler.js
+++ bin/crawler.js
@@ -19,7 +19,7 @@ if(cli_input.length < 1) {
 }
 
 var crawler = new UdemyCrawler({
-    http: {
+    headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
         'upgrade-insecure-requests': 1
     }
